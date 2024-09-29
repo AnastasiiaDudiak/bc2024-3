d8eb49660e052cb4be7ea48ba3c96d395045d776
AnastasiiaDudiak bc2024-3 <ANASTASIIA.DUDIAK@lnu.edu.ua>
2024-09-29 23:14:56 +0300

    Виправлення логіки обробки активів з JSON

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ const program = new Command();
 
 program
     .version('1.0.0')
-    .description('Приклад програми з Commander.js')
-    .requiredOption('-i, --input <path>', 'Шлях до файлу для читання')
-    .option('-o, --output <path>', 'Шлях до файлу для запису результату')
-    .option('-d, --display', 'Вивести результат у консоль')
+    .description('Програма для обробки даних з JSON')
+    .option('-i, --input <path>', 'Шлях до файлу для читання (обов\'язковий)')
+    .option('-o, --output <path>', 'Шлях до файлу для запису (необов\'язковий)')
+    .option('-d, --display', 'Вивести результат у консоль (необов\'язковий)')
     .parse(process.argv);
 
 const options = program.opts();
 
-// Перевірка на обов'язковий параметр
+// Перевірка наявності обов'язкового параметра
 if (!options.input) {
     console.error('Please, specify input file');
     process.exit(1);
 }
 
+// Читання JSON
 let jsonData;
 try {
-    // Читання файлу синхронно
-    const data = fs.readFileSync(options.input, 'utf8');
+    const data = fs.readFileSync(options.input);
     jsonData = JSON.parse(data);
 } catch (err) {
     console.error('Cannot find input file');
@@ -38,21 +38,23 @@ jsonData.forEach(asset => {
 });
 
 // Формат виводу
-const result = `${minAsset.name}: ${minAsset.value}`;
+if (minAsset) {
+    const result = `${minAsset.name}: ${minAsset.value}`;
 
-if (options.display) {
-    console.log(result);
-}
-
-// Запис у файл, якщо вказано
-if (options.output) {
-    try {
-        fs.writeFileSync(options.output, result);
-        console.log(`Результат записано в файл: ${options.output}`);
-    } catch (err) {
-        console.error('Error writing to output file');
-        process.exit(1);
+    if (options.display) {
+        console.log(result);
     }
+
+    if (options.output) {
+        try {
+            fs.writeFileSync(options.output, result);
+            console.log(`Результат записаний у файл: ${options.output}`);
+        } catch (err) {
+            console.error('Помилка запису у вихідний файл');
+        }
+    }
+} else {
+    console.log('Не вдалося знайти активи');
 }
 
 
